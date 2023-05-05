261e77b27e42e0d124ff23588493db9e5fe75ced
Harcharan Singh <honeymankoo22@gmail.com>
2023-05-05 17:29:16 +0530

    Changes in build

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ DOCKER_PUBLIC_IMAGE_NAME=bwalia/whitefalcon
 VERSION=latest
 
 docker build -t ${DOCKER_PUBLIC_IMAGE_NAME}:${VERSION} -f Dockerfile .
+docker login -u $1 -p $2
 docker push ${DOCKER_PUBLIC_IMAGE_NAME}:${VERSION}
