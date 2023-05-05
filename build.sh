60e5b8d86cf33e38d2be26ab295cf677d51969db
Harcharan Singh <honeymankoo22@gmail.com>
2023-05-05 17:35:44 +0530

    Build Changes

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 set -x
+if [ -z "$1" ]; then
+   echo "Docker username is not provided"
+   exit -1
+else
+   echo "Docker username is provided ok"
+fi
+if [ -z "$2" ]; then
+   echo "Docker password is not provided"
+   exit -1
+else
+   echo "Docker password is provided ok"
+fi
 
 DOCKER_PUBLIC_IMAGE_NAME=bwalia/whitefalcon
 VERSION=latest
