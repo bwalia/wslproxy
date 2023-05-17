f306dc6297627c6b3f5e57c40681ef3a7f02af28
Rahul dhelord <phpcoderorg@gmail.com>
2023-05-17 17:03:18 +0530

    ping -- db connection check

--- api/ping.lua
+++ api/ping.lua
@@ -3,6 +3,15 @@ local handle = io.popen("uptime")
 local result = handle:read("*a")
 handle:close()
 
+local redis = require "resty.redis"
+local red = redis:new()
+red:set_timeouts(1000, 1000, 1000) -- 1 sec
+local ok, err = red:connect("redis", 6379)
+local db_connect = true
+if not ok then
+    db_connect = false
+end
+
 local json_str
 local data = {
     app = 'WhiteFalcon',
@@ -10,6 +19,7 @@ local data = {
     stack = "Lua 5.1",
     response = "pong",
     deployment_time = "20230510055429",
+    db_connection = db_connect,
     up_time =  result -- "10:45:05 up  7:44,  0 users,  load average: 1.46, 1.18, 1.02"
 
 }
