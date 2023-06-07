4ec7c9f835fff81ef9a9c70e1dc59f5060d6fdc5
Harcharan Singh <honeymankoo22@gmail.com>
2023-06-07 16:31:02 +0530

    Added footer

--- openresty-admin/src/App.jsx
+++ openresty-admin/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Admin, Resource, Layout } from "react-admin";
 import dataProvider from "./dataProvider";
 import authProvider from "./authProvider";
@@ -7,31 +8,48 @@ import Users from "./Users";
 import Login from "./Login";
 import Servers from "./Servers";
 import Theme from "./Theme";
-import UserIcon from '@mui/icons-material/Group';
-import SessionIcon from '@mui/icons-material/HistoryToggleOff';
-import ServerIcon from '@mui/icons-material/Storage';
-import RuleIcon from '@mui/icons-material/Rule';
+import UserIcon from "@mui/icons-material/Group";
+import SessionIcon from "@mui/icons-material/HistoryToggleOff";
+import ServerIcon from "@mui/icons-material/Storage";
+import RuleIcon from "@mui/icons-material/Rule";
 import Rules from "./Rules";
-import AppBar from "./AppBar"
+import AppBar from "./AppBar";
 
 const API_URL = import.meta.env.VITE_API_URL;
+const deploymentTime = import.meta.env.VITE_DEPLOYMENT_TIME
 
-export const MyLayout = props => <Layout {...props} appBar={AppBar} />;
+export const MyLayout = (props) => <Layout {...props} appBar={AppBar} />;
 
 const App = () => (
-  <Admin
-    loginPage={Login}
-    dataProvider={dataProvider(API_URL)}
-    authProvider={authProvider}
-    dashboard={Dashboard}
-    theme={Theme}
-    layout={MyLayout}
-  >
-    <Resource name="users" {...Users} icon={UserIcon} />
-    <Resource name="sessions" {...Sessions} icon={SessionIcon} />
-    <Resource name="servers" {...Servers} icon={ServerIcon} />
-    <Resource name="rules" {...Rules} icon={RuleIcon} />
-  </Admin>
+  <React.Fragment>
+    <Admin
+      loginPage={Login}
+      dataProvider={dataProvider(API_URL)}
+      authProvider={authProvider}
+      dashboard={Dashboard}
+      theme={Theme}
+      layout={MyLayout}
+    >
+      <Resource name="users" {...Users} icon={UserIcon} />
+      <Resource name="sessions" {...Sessions} icon={SessionIcon} />
+      <Resource name="servers" {...Servers} icon={ServerIcon} />
+      <Resource name="rules" {...Rules} icon={RuleIcon} />
+    </Admin>
+    <div
+      style={{
+        position: "fixed",
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: 100,
+        padding: 6,
+        backgroundColor: "#efefef",
+        textAlign: "center",
+      }}
+    >
+      <p>Deploy at: {deploymentTime}</p>
+    </div>
+  </React.Fragment>
 );
 
 export default App;
