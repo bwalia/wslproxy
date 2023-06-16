2328c79347cb161ac13e5fbf1016143794860887
Balinder WALIA <bwalia@tenthmatrix.co.uk>
2023-06-16 13:02:20 +0200

    Update run.sh to work with local dev docker env
    Update run.sh to work with local dev docker env

--- run.sh
+++ run.sh
@@ -2,23 +2,8 @@
 
 clear
 
-echo "Usage docker-compose up -d instead."
+echo "Running docker-compose up -d."
 
-exit 1
+DOCKER_CONTAINER_NAME="whitefalcon-api"
 
-CONTAINER_ID=whitefalcon-api
-
-echo "Stopping old container ${CONTAINER_ID}"
-docker container stop ${CONTAINER_ID}
-
-echo "Removing old container ${CONTAINER_ID}"
-docker container rm ${CONTAINER_ID}
-#docker image rm bwalia/whitefalcon-api
-
-#docker build -t="bwalia/whitefalcon-api" .
-#--env-file config/env.list \
-#   -v /Users/balinderwalia/Documents/Work/docker/openresty/custom/conf.d:/etc/nginx/conf.d 
-docker run \
--d \
---name ${CONTAINER_ID} \
--p 8080:80 bwalia/whitefalcon-api
+docker exec -it ${DOCKER_CONTAINER_NAME} yarn build
