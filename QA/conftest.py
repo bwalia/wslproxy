6be2a4910321e599e6b8bfbb50c26720c5c516ea
dixa-jangid <dixa.jangid2@gmail.com>
2023-08-07 10:46:12 +0530

    Added conftest file

---
+++ QA/conftest.py
@@ -0,0 +1,39 @@
+import time
+
+import pytest 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+import chromedriver_autoinstaller
+
+
+
+@pytest.fixture(scope="function")
+def setup(request):
+    chrome_driver_path = chromedriver_autoinstaller.install()
+    chrome_service: Service = Service(executable_path=chrome_driver_path)
+
+    chrome_options = webdriver.ChromeOptions()    
+    # Add your options as needed    
+    options = [
+         "--headless",
+         "--disable-gpu",
+         "--no-sandbox",
+    ]
+
+    for option in options:
+        chrome_options.add_argument(option)
+    
+    driver = webdriver.Chrome(options = chrome_options)
+    driver.get("http://int6-api.whitefalcon.io/")
+    driver.find_element(By.NAME, "email").send_keys("ejeyd@example.com")
+    driver.find_element(By.NAME, "password").send_keys("admin")
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    wait = WebDriverWait(driver, 10)
+    wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//button[normalize-space()='Redis']"))).click()
+    # wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".MuiButton-outlined"))).click()
+    request.function.driver = driver
+    yield
+    driver.close()
