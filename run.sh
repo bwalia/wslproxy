515149846cd1c51c6e12c1619148f4c9c744c66d
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-08-19 15:23:42 +0200

    ports revisited to resolved conflicts with webimpetus

--- run.sh
+++ run.sh
@@ -11,3 +11,26 @@ DOCKER_CONTAINER_NAME="whitefalcon"
 docker exec -it ${DOCKER_CONTAINER_NAME} yarn build
 
 docker exec -it ${DOCKER_CONTAINER_NAME} openresty -s reload
+
+HOST_ENDPOINT_UNSECURE_URL="http://localhost:8081"
+curl -IL $HOST_ENDPOINT_UNSECURE_URL
+os_type=$(uname -s)
+
+if [ "$os_type" = "Darwin" ]; then
+open $HOST_ENDPOINT_UNSECURE_URL
+fi
+
+if [ "$os_type" = "Linux" ]; then
+xdg-open $HOST_ENDPOINT_UNSECURE_URL
+fi
+
+HOST_ENDPOINT_UNSECURE_URL="http://localhost:8000"
+curl -IL $HOST_ENDPOINT_UNSECURE_URL
+
+if [ "$os_type" = "Darwin" ]; then
+open $HOST_ENDPOINT_UNSECURE_URL
+fi
+
+if [ "$os_type" = "Linux" ]; then
+xdg-open $HOST_ENDPOINT_UNSECURE_URL
+fi
