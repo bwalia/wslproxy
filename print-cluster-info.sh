0fa80ed11881d99827cb785d6e5de88d186fdcbb
Balinder WALIA <bwalia@tenthmatrix.co.uk>
2023-08-26 12:21:42 +0700

    Print cluster info in workflow for remote workers

---
+++ print-cluster-info.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+
+if [ -z "$1" ]; then
+   echo "Cluster is not provided"
+   exit -1
+else
+   echo "Cluster is provided ok"
+fi
+
+echo "Printing $3 cluster pods, services and ingress related to whitefalcon INT, TEST and ACC environments"
+
+HELM_CMD="helm"
+KUBECTL_CMD="kubectl"
+
+echo "Deploying to $3 cluster"
+# Init kubeconfig for the cluster
+HELM_CMD="helm --kubeconfig /home/bwalia/.kube/vpn-$3.yaml"
+KUBECTL_CMD="kubectl --kubeconfig /home/bwalia/.kube/vpn-$3.yaml"
+
+$KUBECTL_CMD version
+$KUBECTL_CMD cluster-info
+
+$HELM_CMD ls -A | grep whitefalcon
+$KUBECTL_CMD get deploy,svc,pods,ing -A | grep whitefalcon
