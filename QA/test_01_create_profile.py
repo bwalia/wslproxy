fb59ca5638dd0a81005e6d33b6819c1508381c26
dixa-jangid <125278012+dixa-jangid@users.noreply.github.com>
2023-09-28 19:27:23 +0530

    Update test_01_create_profile.py

--- QA/test_01_create_profile.py
+++ QA/test_01_create_profile.py
@@ -1,27 +1,60 @@
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+import chromedriver_autoinstaller
+from chromedriver_autoinstaller import install as install_chrome_driver
+import os
+from selenium.webdriver.common.keys import Keys
+
 
 
 def test_clientIPRule(setup, request):
-    driver = request.function.driver
-    server_name = request.function.server_name
-    targetHost = request.function.targetHost 
+    chrome_driver_path = chromedriver_autoinstaller.install()
+    chrome_service: Service = Service(executable_path=chrome_driver_path)
 
-# Creating rule for allow request when the client IP is matched
-    wait = WebDriverWait(driver, 15)
+    chrome_options = webdriver.ChromeOptions()    
+    # Add your options as needed    
+    options = [
+         "--headless",
+         "--disable-gpu",
+         "--no-sandbox",
+    ]
 
-    def wait_for_element(by, selector):
-      element = wait.until(expected_conditions.presence_of_element_located((by, selector)))
-      return element
+    for option in options:
+        chrome_options.add_argument(option)
+    
+    driver = webdriver.Chrome(options = chrome_options)
+
+    targetHost = os.environ.get('TARGET_HOST')
+    EMAIL = os.environ.get('LOGIN_EMAIL')
+    PASSWORD = os.environ.get('LOGIN_PASSWORD')
+    storageType = os.environ.get('STORAGE_TYPE')
+
+        
+    driver.get(targetHost)
+    driver.find_element(By.NAME, "email").send_keys(EMAIL)
+    driver.find_element(By.NAME, "password").send_keys(PASSWORD)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    
+    if storageType == "redis":
+        time.sleep(4)
+        driver.find_element(By.XPATH, "//button[normalize-space()='Redis']").click()
+        print("Executing test on Redis data storage")
+    elif storageType == "disk": 
+        time.sleep(4)
+        driver.find_element(By.XPATH, "//button[normalize-space()='Disk']").click()
+        print("Executing test on Disk data storage")
 
 
-    wait_for_element(By.XPATH, "//a[@href='#/profiles']").click()
+
+    driver.find_element(By.XPATH, "//a[@href='#/profiles']").click()
     time.sleep(2)
-    wait_for_element(By.XPATH, "//a[@href='#/profiles/create']").click()
-    wait_for_element(By.ID, "name").send_keys("qa_test")
-
-    wait_for_element(By.XPATH, "//button[@type='submit']").click()
-    wait_for_element(By.XPATH, "//button[@aria-label='Sync API Storage']").click()
+    driver.find_element(By.XPATH, "//a[@href='#/profiles/create']").click()
+    driver.find_element(By.ID, "name").send_keys("qa_test")
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+    driver.find_element(By.XPATH, "//button[@aria-label='Sync API Storage']").click()
     time.sleep(2)
+    driver.close
