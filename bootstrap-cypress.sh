f2f63b4b306a591e2007a6d604fe8a2082d37b5d
Balinder Walia <92169035+bwalia-sciensano@users.noreply.github.com>
2023-12-06 18:02:22 +0100

    Cypress tests added

--- bootstrap-cypress.sh
+++ bootstrap-cypress.sh
@@ -7,16 +7,17 @@ PASSWD="$2"
 TARGET_ENV="$3"
 
 rm -Rf .env
-echo "" > env_cypress
-echo "CYPRESS_login_username=$USERNAME" >> env_cypress
-echo "CYPRESS_login_password=$PASSWD" >> env_cypress
-echo "CYPRESS_TARGET_ENV=$TARGET_ENV" >> env_cypress
-echo "" >> env_cypress
-mv env_cypress /tmp/.env_cypress
-mv docker-compose-cypress.yml /tmp/docker-compose-cypress.yml
-mv cypress /tmp/cypress
+rm -Rf /tmp/.env_cypress
+echo "" > /tmp/.env_cypress
+echo "CYPRESS_LOGIN_EMAIL=$USERNAME" >> /tmp/.env_cypress
+echo "CYPRESS_LOGIN_PASSWORD=$PASSWD" >> /tmp/.env_cypress
+echo "CYPRESS_TARGET_ENV=$TARGET_ENV" >> /tmp/.env_cypress
+echo "" >> /tmp/.env_cypress
+
+# mv docker-compose-cypress.yml /tmp/docker-compose-cypress.yml
+# mv cypress /tmp/cypress
 mv /tmp/.env_cypress .env
-mv /tmp/docker-compose-cypress.yml docker-compose-cypress.yml
-mv /tmp/cypress cypress
-ls -al
+# mv /tmp/docker-compose-cypress.yml docker-compose-cypress.yml
+# mv /tmp/cypress cypress
+# ls -al
 docker compose -f docker-compose-cypress.yml up
