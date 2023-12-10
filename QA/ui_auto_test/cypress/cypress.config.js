e9914a27989f0423041c6c3f07190f83bd7a5f64
dixa-jangid <dixa.jangid2@gmail.com>
2023-12-10 18:58:11 +0530

    cypress test

---
+++ QA/ui_auto_test/cypress/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      chromeWebSecurity: false;
+      // implement node event listeners here
+      // pageLoadTimeout: 100000;
+    },
+    supportFile: false
+  },
+});
---
+++ QA/ui_auto_test/qa-bootstrap-cypress.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+USERNAME="$1"
+PASSWD="$2"
+TARGET_ENV="$3"
+
+rm -Rf .env
+rm -Rf /tmp/.env_cypress
+echo "" > /tmp/.env_cypress
+echo "CYPRESS_LOGIN_EMAIL=$USERNAME" >> /tmp/.env_cypress
+echo "CYPRESS_LOGIN_PASSWORD=$PASSWD" >> /tmp/.env_cypress
+echo "CYPRESS_TARGET_ENV=$TARGET_ENV" >> /tmp/.env_cypress
+if [ "$TARGET_ENV" = "int" ]; then
+    echo "CYPRESS_BASE_URL=https://api.int2.whitefalcon.io" >> /tmp/.env_cypress
+    echo "CYPRESS_FRONTEND_URL=https://front.int2.whitefalcon.io" >> /tmp/.env_cypress
+    echo "CYPRESS_NODEAPP_ORIGIN_HOST=10.43.140.53" >> /tmp/.env_cypress
+    echo "CYPRESS_SERVER_NAME=front.int2.whitefalcon.io" >> /tmp/.env_cypress
+    echo "CYPRESS_TARGET_PLATFORM=kubernates" >> /tmp/.env_cypress
+elif [ "$TARGET_ENV" = "local" ]; then
+    echo "CYPRESS_BASE_URL=http://host.docker.internal:8081" >> /tmp/.env_cypress
+    echo "CYPRESS_FRONTEND_URL=http://host.docker.internal:8000" >> /tmp/.env_cypress
+    echo "CYPRESS_NODEAPP_ORIGIN_HOST=172.177.0.10:3009" >> /tmp/.env_cypress
+    echo "CYPRESS_SERVER_NAME=host.docker.internal" >> /tmp/.env_cypress
+    echo "CYPRESS_TARGET_PLATFORM=docker" >> /tmp/.env_cypress
+fi
+
+echo "" >> /tmp/.env_cypress
+
+# mv docker-compose-cypress.yml /tmp/docker-compose-cypress.yml
+# mv cypress /tmp/cypress
+mv /tmp/.env_cypress .env
+# mv /tmp/docker-compose-cypress.yml docker-compose-cypress.yml
+# mv /tmp/cypress cypress
+# ls -al
+docker-compose -f qa-docker-compose-cypress.yml up
