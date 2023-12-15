4477096e14d60e156d3b919f41a57ffa8dd8aaed
Harcharan Singh <harcharansingh@Harcharans-MacBook-Pro.local>
2023-12-15 17:21:07 +0530

    Update env vars for different envs

--- build.sh
+++ build.sh
@@ -41,19 +41,35 @@ TARGET_DOCKER_COMPOSE_FILE="docker-compose.yml"
 
 if [ "$TARGET_ENV_NAME" == "dev" ]; then
     TARGET_DOCKER_COMPOSE_FILE="docker-compose.yml"
+    API_URL="http://localhost:8080/api"
+    FRONT_URL="http://localhost:8000"
 elif [ "$TARGET_ENV_NAME" == "syndev" ]; then
     TARGET_DOCKER_COMPOSE_FILE="docker-compose-${TARGET_ENV_NAME}.yml"
+    API_URL="http://localhost:8080/api"
+    FRONT_URL="http://localhost:8000"
 elif [ "$TARGET_ENV_NAME" == "syntest" ]; then
     TARGET_DOCKER_COMPOSE_FILE="docker-compose-${TARGET_ENV_NAME}.yml"
+    API_URL="http://localhost:8088/api"
+    FRONT_URL="http://localhost:8008"
 elif [ "$TARGET_ENV_NAME" == "synacc" ]; then
     TARGET_DOCKER_COMPOSE_FILE="docker-compose-${TARGET_ENV_NAME}.yml"
+    API_URL="http://localhost:8090/api"
+    FRONT_URL="http://localhost:8009"
 elif [ "$TARGET_ENV_NAME" == "synprod" ]; then
     TARGET_DOCKER_COMPOSE_FILE="docker-compose-${TARGET_ENV_NAME}.yml"
+    API_URL="http://localhost:8095/api"
+    FRONT_URL="http://localhost:8091"
 fi
+
 TARGET_ENV_FILE=".env.docker"
 TARGET_NODE_APP_ENV_FILE=".env.nodeapp"
 echo "" >> $TARGET_ENV_FILE
 echo "JWT_SECURITY_PASSPHRASE=$JWT_TOKEN_KEY" >> $TARGET_ENV_FILE
+echo "REDIS_HOST=$DOCKER_CONTAINER_NAME-redis" >> $TARGET_ENV_FILE
+echo "APP_NAME=$DOCKER_CONTAINER_NAME" >> $TARGET_ENV_FILE
+echo "API_URL=$API_URL" >> $TARGET_ENV_FILE
+echo "FRONT_URL=$FRONT_URL" >> $TARGET_ENV_FILE
+
 rm -rf $TARGET_NODE_APP_ENV_FILE
 touch $TARGET_NODE_APP_ENV_FILE
 echo "JWT_SECRET_KEY=$JWT_TOKEN_KEY" >> $TARGET_NODE_APP_ENV_FILE
