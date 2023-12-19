f7c8e77f066248100e12c5205d973d3cd83958d0
Dixa's Workspace <Dixa@192.168.1.5>
2023-12-19 20:39:53 +0530

    Cypress congif  update

--- QA/cypress_ui_tests/cypress.config.js
+++ QA/cypress_ui_tests/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = {
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
