a16459ddf3111a02f93f79e7a4a7f05fda271dcf
Dixa's Workspace <Dixa@192.168.1.5>
2023-12-19 20:00:40 +0530

    Cypress UI workflow update

--- QA/cypress_ui_tests/qa-bootstrap-cypress.sh
+++ QA/cypress_ui_tests/qa-bootstrap-cypress.sh
@@ -7,37 +7,39 @@ LOGIN_PASSWORD=$2
 TARGET_ENV=$3
 JWT_TOKEN_KEY=$4
 
-rm -Rf .env
-rm -Rf /tmp/.env_cypress
-echo "" > /tmp/.env_cypress
-echo "CYPRESS_LOGIN_EMAIL=$LOGIN_EMAIL" >> /tmp/.env_cypress
-echo "CYPRESS_LOGIN_PASSWORD=$LOGIN_PASSWORD" >> /tmp/.env_cypress
-echo "CYPRESS_TARGET_ENV=$TARGET_ENV" >> /tmp/.env_cypress
-echo "CYPRESS_JWT_TOKEN_KEY=$JWT_TOKEN_KEY" >> /tmp/.env_cypress
-
 if [ "$TARGET_ENV" = "int" ]; then
     BASE_URL="https://api.int2.whitefalcon.io"
     FRONTEND_URL="https://front.int2.whitefalcon.io"
     NODEAPP_ORIGIN_HOST="10.43.140.53:3009"
     SERVER_NAME="front.int2.whitefalcon.io"
     TARGET_PLATFORM="kubernetes"
+    ENV_FILE=".env_cypress_int"
 fi
-if [ "$TARGET_ENV" = "local" ]; then
+if [ "$TARGET_ENV" = "dockerinternal" ]; then
     BASE_URL="http://host.docker.internal:8081"
     FRONTEND_URL="http://host.docker.internal:8000"
     NODEAPP_ORIGIN_HOST="172.177.0.10:3009"
     SERVER_NAME="host.docker.internal"
     TARGET_PLATFORM="docker"
+    ENV_FILE=".env_cypress_docker_internal"
 fi
+rm -Rf .env
+rm -Rf /tmp/$ENV_FILE
+echo "" > /tmp/$ENV_FILE
+
 sleep 2
-echo "CYPRESS_BASE_PUB_URL=$BASE_URL" >> /tmp/.env_cypress
-echo "CYPRESS_FRONTEND_URL=$FRONTEND_URL" >> /tmp/.env_cypress
-echo "CYPRESS_NODEAPP_ORIGIN_HOST=$NODEAPP_ORIGIN_HOST" >> /tmp/.env_cypress
-echo "CYPRESS_SERVER_NAME=$SERVER_NAME" >> /tmp/.env_cypress
-echo "CYPRESS_TARGET_PLATFORM=$TARGET_PLATFORM" >> /tmp/.env_cypress
+echo "CYPRESS_LOGIN_EMAIL=$LOGIN_EMAIL" >> /tmp/$ENV_FILE
+echo "CYPRESS_LOGIN_PASSWORD=$LOGIN_PASSWORD" >> /tmp/$ENV_FILE
+echo "CYPRESS_TARGET_ENV=$TARGET_ENV" >> /tmp/$ENV_FILE
+echo "CYPRESS_JWT_TOKEN_KEY=$JWT_TOKEN_KEY" >> /tmp/$ENV_FILE
+echo "CYPRESS_BASE_PUB_URL=$BASE_URL" >> /tmp/$ENV_FILE
+echo "CYPRESS_FRONTEND_URL=$FRONTEND_URL" >> /tmp/$ENV_FILE
+echo "CYPRESS_NODEAPP_ORIGIN_HOST=$NODEAPP_ORIGIN_HOST" >> /tmp/$ENV_FILE
+echo "CYPRESS_SERVER_NAME=$SERVER_NAME" >> /tmp/$ENV_FILE
+echo "CYPRESS_TARGET_PLATFORM=$TARGET_PLATFORM" >> /tmp/$ENV_FILE
 
-echo "" >> /tmp/.env_cypress
+echo "" >> /tmp/$ENV_FILE
 
-mv /tmp/.env_cypress .env
+mv /tmp/$ENV_FILE .env
 cat .env
 docker compose -f qa-docker-compose-cypress.yml up --remove-orphans
