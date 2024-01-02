020f81667359aa6a6c6286acadc952e65911d035
Dixa's Workspace <Dixa@Dixas-MacBook-Air.local>
2024-01-02 19:21:37 +0530

    permission error for downloads folder

--- QA/cypress_ui_tests/cypress.config.js
+++ QA/cypress_ui_tests/cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = {
-  // reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
+    downloadsFolder : 'cypress/download	',
     setupNodeEvents(on, config) {
-      // require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
